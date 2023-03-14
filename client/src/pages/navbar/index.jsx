253d2e24118cb827c35ee02b1e84a7f856f66bae
Sam Sumiya <sams@samsumiya.com>
2023-03-13 17:27:32 -0700

    conditonally render go back button based on url

--- client/src/pages/navbar/index.jsx
+++ client/src/pages/navbar/index.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useContext } from 'react';
-import
-{
+import React, { useState, useContext } from 'react'
+import {
   Box,
   IconButton,
   useTheme,
@@ -8,36 +7,30 @@ import
   Typography,
   Button,
   AppBar,
-} from '@mui/material';
-import { DarkMode, LightMode } from '@mui/icons-material';
-import { colorTokens } from 'theme';
-import { useNavigate } from 'react-router-dom';
-import { ThemeContext } from 'App';
-import { Link } from 'react-router-dom';
-import MainAppBar from '../../components/MainAppBar'; 
-const pages = [ 'Write a post', 'All Authors', '' ];
+} from '@mui/material'
+import { DarkMode, LightMode } from '@mui/icons-material'
+import { colorTokens } from 'theme'
+import { useNavigate } from 'react-router-dom'
+import { ThemeContext } from 'App'
+import { Link } from 'react-router-dom'
+import MainAppBar from '../../components/MainAppBar'
+const pages = ['Write a post', 'All Authors', '']
 
-const Navbar = () =>
-{
-  const { name } = useContext( ThemeContext );
-  const [ darkTheme, setDarkTheme ] = name;
-  const yellow = colorTokens.lightMode.light;
-  const isNonMobileScreens = useMediaQuery( '(min-width: 1000px)' );
-  const navigate = useNavigate();
+const Navbar = () => {
+  const { name } = useContext(ThemeContext)
+  const [darkTheme, setDarkTheme] = name
+  const yellow = colorTokens.lightMode.light
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
+  const navigate = useNavigate()
 
   return (
     <Box sx={{ marginTop: 2, marginBottom: 2 }} width="100%">
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignitem="center"
-        gap="1rem"
-      >
+      <Box display="flex" justifyContent="center" alignitem="center" gap="1rem">
         <Typography
           fontSize="clamp(2rem, 3rem, 3.25rem)"
           fontWeight="bold"
           color="#3d5afe"
-          onClick={() => navigate( '/' )}
+          onClick={() => navigate('/')}
           sx={{
             '&:hover': {
               cursor: 'pointer',
@@ -48,7 +41,7 @@ const Navbar = () =>
         </Typography>
         <IconButton
           onClick={() =>
-            darkTheme === 'dark' ? setDarkTheme( 'light' ) : setDarkTheme( 'dark' )
+            darkTheme === 'dark' ? setDarkTheme('light') : setDarkTheme('dark')
           }
         >
           {darkTheme === 'dark' ? (
@@ -58,11 +51,9 @@ const Navbar = () =>
           )}
         </IconButton>
       </Box>
-      {isNonMobileScreens ? (
-        <MainAppBar/>
-      ) : null}
+      {isNonMobileScreens ? <MainAppBar /> : null}
     </Box>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
